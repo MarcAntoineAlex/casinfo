d3f399a0e9f6632ac0f473e41700b05e7731252d
MarcAntoineAlex <18510756258@163.com>
2021-09-24 10:30:16 +0800

    c

--- architect1.py
+++ architect1.py
@@ -24,9 +24,12 @@ class Architect(object):
                                           weight_decay=args.arch_weight_decay)
 
     def critere(self, pred, true, data_count, reduction='mean'):
+        if reduction != 'mean':
+            crit = nn.MSELoss(reduction=reduction)
+            return crit(pred * self.teacher.arch[data_count:data_count + pred.shape[0]] ** 0.5,
+                                  true * self.teacher.arch[data_count:data_count + pred.shape[0]] ** 0.5).mean(dim=-1)
         return self.criterion(pred * self.teacher.arch[data_count:data_count + pred.shape[0]] ** 0.5,
-                              true * self.teacher.arch[data_count:data_count + pred.shape[0]] ** 0.5,
-                              reduction=reduction)
+                                  true * self.teacher.arch[data_count:data_count + pred.shape[0]] ** 0.5)
 
     def _compute_unrolled_model(self, input_data, eta, teacher_optimizer, data_count):
         pred, true = self._process_one_batch(input_data, self.teacher)
