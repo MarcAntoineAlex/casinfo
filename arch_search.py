40ef3d44866c1e72643144829c0ed689c4154f3d
MarcAntoineAlex <18510756258@163.com>
2021-10-11 17:29:36 +0800

    weight picture

--- arch_search.py
+++ arch_search.py
@@ -238,9 +238,9 @@ def train(trn_loader, val_loader, unl_loader, test_loader, teacher, assistant, s
             unl_iter = iter(unl_loader)
             unl_data = next(unl_iter)
 
-        implicit_grads = architect.step_all3(trn_data, val_data, unl_data, lr, optimizer_t, optimizer_a, optimizer_s, args.unrolled, data_count)
+        # implicit_grads = architect.step_all3(trn_data, val_data, unl_data, lr, optimizer_t, optimizer_a, optimizer_s, args.unrolled, data_count)
 
-        STAT_arch_grad.append(implicit_grads[0].mean().item())
+        # STAT_arch_grad.append(implicit_grads[0].mean().item())
         STAT_arch.append(teacher.architect_param123.mean().item())
         STAT_arch_std.append(teacher.architect_param123.std().item())
 
