67a0d971752388971dc8ed5e6a85c4020f91b5c6
MarcAntoineAlex <18510756258@163.com>
2021-10-29 10:27:29 +0800

    k

--- util/tools.py
+++ util/tools.py
@@ -42,13 +42,13 @@ class EarlyStopping:
         self.delta = delta
         self.rank = rank
 
-    def __call__(self, val_loss, model, path, ii):
+    def __call__(self, val_loss, model, path):
         if self.early_stop:
             return
         score = -val_loss
         if self.best_score is None:
             self.best_score = score
-            self.save_checkpoint(val_loss, model, path, ii)
+            self.save_checkpoint(val_loss, model, path)
         elif score < self.best_score + self.delta:
             self.counter += 1
             print(f'EarlyStopping counter: {self.counter} out of {self.patience}')
@@ -56,7 +56,7 @@ class EarlyStopping:
                 self.early_stop = True
         else:
             self.best_score = score
-            self.save_checkpoint(val_loss, model, path, ii)
+            self.save_checkpoint(val_loss, model, path)
             self.counter = 0
 
     def save_checkpoint(self, val_loss, model, path):
